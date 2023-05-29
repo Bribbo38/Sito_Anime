3653d672ea5bf7eb21670e2a509801fdc4d3988d
Bribbo38 <luca06zando@gmail.com>
2023-05-29 14:51:03 +0200

    On branch master
    Commit iniziale con tutti i file
    
    Cambiamenti da caricare:
        new file:   .gitignore
    	new file:   Altro/Stile sito.txt
    	new file:   Altro/archivio + preferiti + ultime uscite.png
    	new file:   Altro/index stile.png
    	new file:   Altro/pagine anime.png
    	new file:   Img/Kaguya3.png
    	new file:   Img/Komi.png
    	new file:   Img/Skeleton.png
    	new file:   Img/Spyx.png
    	new file:   Img/TheRising2.png
    	new file:   Img/background-side.jpg
    	new file:   Img/background.jpg
    	new file:   Img/banner.jpg
    	new file:   Img/list.png
    	new file:   Img/new.png
    	new file:   Img/random.png
    	new file:   Img/thumb.png
    	new file:   Js/icon.js
    	new file:   Js/script.js
    	new file:   Js/sidebar.js
    	new file:   Style/anime-page.css
    	new file:   Style/index.css
    	new file:   Style/responsive.css
    	new file:   Style/style.css
    	new file:   base-anime.html
    	new file:   index.html

---
+++ Js/sidebar.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("burgerNav").style.width = "100%";
+    document.getElementById("burgerIcon").style.rotate = "90deg";
+}
+
+function closeNav() {
+    document.getElementById("burgerNav").style.width = "0%";
+    document.getElementById("burgerIcon").style.rotate = "0deg";
+}
